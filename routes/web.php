05e84311c1ca1c6ec40aeea178ccd01a5f1ba8b2
kylariaaa <shikitao900@gmail.com>
2025-11-12 14:43:17 +0700

    P2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Livewire\Beranda;
+use App\Livewire\User;
+use App\Livewire\Laporan;
+use App\Livewire\Produk;
+Use App\Livewire\Transaksi;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', Beranda::class)->middleware(['auth'])->name('home');
+Route::get('/user', user::class)->middleware(['auth'])->name('user');
+Route::get('/laporan', Laporan::class)->middleware(['auth'])->name('laporan');
+Route::get('/produk', Produk::class)->middleware(['auth'])->name('produk');
+Route::get('/transaksi', Transaksi::class)->middleware(['auth'])->name('transaksi');
