135a252e8ea87b6c0be7816d60c4c0eb85403b3f
kylariaaa <shikitao900@gmail.com>
2025-11-13 08:57:19 +0700

    P3

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -7,12 +7,42 @@ use App\Models\user as ModelUser;
 
 class User extends Component
 {
-    public $pilihanMenu = 'lihat';
-    public function pilihMenu($menu)
-    {
-        $this->pilihanMenu = $menu;
-    }
+    public $pilihanMenu = "lihat";
+    public $nama;
+    public $email;
+    public $peran;
+    public $password;
 
+        public function simpan(){
+            $this->validate([
+                'nama' => 'required',
+                'email' => ['required','email','unique:users,email'],
+                'password' => 'required',
+                'peran' => 'required'
+            ],[
+                'nama.required' => 'Nama Harus Diisi',
+                'email.required' => 'Email Harus Diisi',
+                'email.email' => 'Format mesti diisi',
+                'email.unique' => 'Email sudah digunakan',
+                'peran.required' => 'Peran Harus Diisi',
+                'password.required' => 'Password Harus Diisi',
+            ]);
+
+            $simpan = new ModelUser();
+            $simpan->name = $this->nama;
+            $simpan->email = $this->email;
+            $simpan->password = bcrypt($this->password);
+            $simpan->peran = $this->peran;
+            $simpan->save();
+
+            $this->reset('nama', 'email', 'password', 'peran');
+            $this->pilihanMenu = 'lihat';
+
+        }
+        public function pilihMenu($menu)
+        {
+            $this->pilihanMenu = $menu;
+        }
     public function render()
     {
         return view('livewire.user')->with ([
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -73,24 +73,19 @@
                             <label>Nama</label>
                             <input type="text" class="form-control" wire:model='nama' />
                             @error('nama')
-                            <span class="text-danger">Nama Harus diisi</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
 
                             <label>Email</label>
                             <input type="email" class="form-control" wire:model='email' />
                             @error('email')
-                            <span class="text-danger">Email Harus diisi</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
 
                             <label>Password</label>
                             <input type="password" class="form-control" wire:model='password' />
                             @error('password')
-                            <span class="text-danger">Password Harus diisi</span>
-                            @enderror
-                            <label>Password</label>
-                            <input type="password" class="form-control" wire:model='password' />
-                            @error('password')
-                            <span class="text-danger">Password Harus diisi</span>
+                            <span class="text-danger">{{ $message }}</span>
                             @enderror
 
                             <label>Peran</label>
